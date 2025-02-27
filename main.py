a7bda4bd4663ff96f0ede0fb50affcfc249d75bd
mmerkulov <misha.merkulov@gmail.com>
2025-02-27 17:38:44 +0100

    add Bank

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ CARDS_SET = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Туз', 'Корол
 # По сути масти не важны и cards_set не нужен, хватает карты и её масти. ((4*13) * x), где x от 1 и до 4, x - число колодо участвующих в игре
 denomination = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'Король': 10, 'Королева': 10, 'Валет': 10, 'Туз': 11}
 
+
 def generate_deck(suit: list, card_set: list) -> list:
     deck = [(s, c) for s in suit for c in card_set]
     random.shuffle(deck)
@@ -32,46 +33,74 @@ def calculate_hand(deck: list) -> int:
         sum -= 10
     return sum
 
-def equal_result(sum_player: int, sum_dealer: int) -> str:
+def equal_result(sum_player: int, sum_dealer: int) -> int:
+    print(f'Счёт игрока - {sum_player}')
+    print(f'Счёт дилера - {sum_dealer}')
     if sum_player > 21:
-        return 'Player проиграл, сожалеем.'
+        print('Player проиграл, сожалеем.')
+        return 0
     elif sum_dealer > 21:
-        return 'Dealer проиграл, поздравляем с победой.'
+        print('Dealer проиграл, поздравляем с победой.')
+        return 1
     elif sum_player > sum_dealer:
-        return f'Player победил, поздравляем, дилер проиграл. Результат: {sum_player} > {sum_dealer}'
+        print(f'Player победил, поздравляем, дилер проиграл. Результат: {sum_player} > {sum_dealer}')
+        return 1
     elif sum_player == sum_dealer:
-        return f'Ничья! Результат: {sum_player} = {sum_dealer}'
+        print(f'Ничья! Результат: {sum_player} = {sum_dealer}')
+        return 2
     else:
-        return 'Дилер победил! Сожалеем.'
+        print('Дилер победил! Сожалеем.')
+        return 0
 
-def put_money_in_bank():
+def calculate_player_bank(player, bid: int):
     pass
 
 def play_blackjack():
     print(f'!!!Приветствую в игре BlackJack!!!')
-    start_deck = generate_deck(suit=SUIT, card_set=CARDS_SET)
-    player_hand = [start_deck.pop(), start_deck.pop()]
-    dealer_hand = [start_deck.pop(), start_deck.pop()]
 
-    # Ход игрока
-    while calculate_hand(player_hand) < 21:
-        print(f'Ваша рука: {player_hand}')
-        action = input('Ваш ход, будете брать ещё карту? (more/enogh): ').lower()
-        if action == 'more':
-            player_hand.append(start_deck.pop())
-            print(f'У вас на руках: {player_hand}, ваш счёт: {calculate_hand(player_hand)}')
-        elif action == 'enogh':
-            break
-        else:
-            print(f'Команда не понятна, введите одну из следующих (mehr, enogh')
+    players_bank = float(input('На сколько будете играть? Ввевдите сумму (это общая сумма, которая у вас есть на начало игры) = '))
+
+    is_play_again = True
+    while players_bank > 0 and is_play_again:
+        bid = int(input('Введите ставку игры - '))
+        start_deck = generate_deck(suit=SUIT, card_set=CARDS_SET)
+        player_hand = [start_deck.pop(), start_deck.pop()]
+        dealer_hand = [start_deck.pop(), start_deck.pop()]
+
+        # Ход игрока
+        while calculate_hand(player_hand) < 21:
+            print(f'Ваша рука: {player_hand}')
+            action = input('Ваш ход, будете брать ещё карту? (more/enogh/+/-): ').lower()
+            if action in ('more', '+'):
+                player_hand.append(start_deck.pop())
+                print(f'У вас на руках: {player_hand}, ваш счёт: {calculate_hand(player_hand)}')
+            elif action in ('enogh', '-'):
+                break
+            else:
+                print(f'Команда не понятна, введите одну из следующих (mehr, enogh')
+                continue
+
+            # Ход дилера
+        while calculate_hand(dealer_hand) < 17:
+            dealer_hand.append(start_deck.pop())
+            print(f'Рука дилера: {dealer_hand}')
+
+        # Считаем результаты
+        x = equal_result(sum_player=calculate_hand(player_hand), sum_dealer=calculate_hand(dealer_hand))
+        if x == 1:
+            players_bank += bid
+        elif x == 2:
             continue
+        else:
+            players_bank -= bid
 
-    # Ход дилера
-    while calculate_hand(dealer_hand) < 17:
-        dealer_hand.append(start_deck.pop())
+        print(f'Ваш банк = {players_bank}')
+        question = input('Сыграть ещё раз? (+/-/Yes/No): ')
+        if question in ('+', 'Yes'):
+            is_play_again = True
+        else:
+            is_play_again = False
 
-    # Считаем результаты
-    print(equal_result(sum_player=calculate_hand(player_hand), sum_dealer=calculate_hand(dealer_hand)))
 
 def give_up():
     # сдаться
